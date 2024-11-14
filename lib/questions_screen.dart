81ed17cda2f236057c3cf16059cc2b22b53f4a38
Hannah Z <eveline1129@163.com>
2024-11-14 17:07:30 -0600

    added getShuffledAnswers

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -17,23 +17,28 @@ class _QuestionsSate extends State<QuestionsScreen> {
     final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(color: Colors.white),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          ...currentQuestion.answers.map((item) {
-            return AnswerButton(
-              text: item,
-              onTap: () {},
-            );
-          })
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffledAnswers().map((item) {
+              return AnswerButton(
+                text: item,
+                onTap: () {},
+              );
+            })
+          ],
+        ),
       ),
     );
   }
