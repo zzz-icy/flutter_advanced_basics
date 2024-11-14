04856171e61818277f71034055e666e106e6b41e
Hannah Z <eveline1129@163.com>
2024-11-14 00:34:51 -0600

    mapping list for questions

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
 import 'package:flutter/material.dart';
 
 class QuestionsScreen extends StatefulWidget {
@@ -13,21 +14,25 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsSate extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
+    final currentQuestion = questions[0];
+    return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
-            'The questions...',
-            style: TextStyle(color: Colors.white),
+            currentQuestion.text,
+            style: const TextStyle(color: Colors.white),
           ),
-          SizedBox(
+          const SizedBox(
             height: 30,
           ),
-          AnswerButton('11'),
-          AnswerButton('11'),
-          AnswerButton('11'),
+          ...currentQuestion.answers.map((item) {
+            return AnswerButton(
+              text: item,
+              onTap: () {},
+            );
+          })
         ],
       ),
     );
